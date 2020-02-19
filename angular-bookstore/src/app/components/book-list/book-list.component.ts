a00486f7d9e850971718ba613ba24b9f5d44ef07
Dany Hasiando Panggabean <dany_panggabean@bca.co.id>
2020-02-19 11:37:04 +0700

    Created angular project and completed the feature 1

---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,24 @@
+import { BookService } from './../../services/book.service';
+import { Book } from './../../common/book';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Book[];
+  constructor(private _bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.listBooks();
+  }
+
+  listBooks() {
+    this._bookService.getBooks().subscribe(
+      data => this.books = data
+    )
+  }
+}
--- onlinebookstore/src/main/java/com/danyhp/developer/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/danyhp/developer/onlinebookstore/repository/BookRepository.java
@@ -2,8 +2,8 @@ package com.danyhp.developer.onlinebookstore.repository;
 
 import com.danyhp.developer.onlinebookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
